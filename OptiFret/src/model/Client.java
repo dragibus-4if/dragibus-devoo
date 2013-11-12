8e22aa7596cc471bbec5c6676973b4dae0d0deca
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-11-12 17:58:54 +0100

    Implémentation de Client

--- OptiFret/src/model/Client.java
+++ OptiFret/src/model/Client.java
@@ -5,5 +5,20 @@ package model;
  * @author jmcomets
  */
 class Client {
-    
+
+    private Long id;
+    private String nom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
 }
