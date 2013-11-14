6048781c4c2fd77f8c3f7a83f632112ac258e384
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-11-14 09:04:44 +0100

    UML avancé

---
+++ OptiFret-généré/src/model/ChocoTSPStrategy.java
@@ -0,0 +1,23 @@
+package model;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.E9950BEA-8652-552A-27D8-DE0A2E671358]
+// </editor-fold> 
+public class ChocoTSPStrategy implements TSPStrategy {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.DA7F8D16-4E00-9363-8E89-09AA73E9E9E1]
+    // </editor-fold> 
+    public ChocoTSPStrategy () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.FBBC6311-F81D-7312-9961-53F05DD71AA8]
+    // </editor-fold> 
+    public List<RoadNode> execute (List<RoadNode> nodes) {
+        return null;
+    }
+
+}
+
---
+++ OptiFret-généré/src/model/TimeSlot.java
@@ -0,0 +1,26 @@
+package model;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.7B786DD0-9399-7D57-E0C0-5924748F50DF]
+// </editor-fold> 
+public class TimeSlot {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.E9461AD4-F079-2F4D-367C-EAEC914DF170]
+    // </editor-fold> 
+    private Date begin;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.F7559EE2-07CC-5CB2-D617-16BE162AB837]
+    // </editor-fold> 
+    private Date end;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.77F11FDB-E5A5-2C9F-C7EF-2252554B44B2]
+    // </editor-fold> 
+    public TimeSlot () {
+    }
+
+}
+
