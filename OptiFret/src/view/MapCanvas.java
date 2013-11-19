016864af8c3022682425856505c983b5bbc85ada
Sylvain Abadie <s.abadie@kreactive.com>
2013-11-19 08:23:27 +0100

    MapCanvas updated

--- OptiFret/src/view/MapCanvas.java
+++ OptiFret/src/view/MapCanvas.java
@@ -4,9 +4,12 @@
  */
 package view;
 
+import java.awt.BasicStroke;
 import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -20,13 +23,17 @@ public class MapCanvas extends Canvas {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        draw(g);
+        ArrayList <ArcView> temp=new ArrayList<ArcView> ();
+        draw(g,temp);
     }
     
-    private void draw(Graphics g) {
+    private void draw(Graphics g,ArrayList<ArcView> arcs) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRect(0,0,getWidth(),getHeight());
-        g2d.drawString("Java 2D", 0, 0);
+        g2d.drawString("Java 2D", 50, 50);
+        g2d.drawOval(10, 10, 20, 20);
+        g2d.setStroke(new BasicStroke(5));
+        g2d.drawLine(0, 0, getWidth(), getHeight());
+        g2d.drawRect(0,0,getWidth()-1,getHeight()-1);
 
     }
 }
