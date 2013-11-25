01f27a4df28aac565a46237ac36375c47b4ade28
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-11-25 19:15:14 +0100

    Fix dragging

--- OptiFret/src/view/NavigablePanel.java
+++ OptiFret/src/view/NavigablePanel.java
@@ -1,8 +1,6 @@
 package view;
 
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -19,20 +17,15 @@ public abstract class NavigablePanel extends JPanel {
     private static final double SCALE_MAX = 3;
     private static final double SCALE_MIN = 0.1;
     private static final double SCALE_INC = 0.1;
-    private static final double SCROLL_SPEED = 5;
 
     private double scale;
-
-    private double vX;
-    private double vY;
-    private double offX;
-    private double offY;
+    
+    private int offX;
+    private int offY;
 
     private boolean dragging;
     private int grabX;
     private int grabY;
-    private int dragX;
-    private int dragY;
 
     public NavigablePanel() {
         super();
@@ -76,71 +69,17 @@ public abstract class NavigablePanel extends JPanel {
                 doScroll(e);
             }
         });
-        addKeyListener(new KeyListener() {
-
-            @Override
-            public void keyTyped(KeyEvent e) {
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                int keyCode = e.getKeyCode();
-                switch (keyCode) {
-                    case KeyEvent.VK_UP:
-                        vY += SCROLL_SPEED;
-                        break;
-
-                    case KeyEvent.VK_DOWN:
-                        vY -= SCROLL_SPEED;
-                        break;
-
-                    case KeyEvent.VK_LEFT:
-                        vX += SCROLL_SPEED;
-                        break;
-
-                    case KeyEvent.VK_RIGHT:
-                        vX -= SCROLL_SPEED;
-                        break;
-                }
-                updateOffset();
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                int keyCode = e.getKeyCode();
-                switch (keyCode) {
-                    case KeyEvent.VK_UP:
-                        vY += -SCROLL_SPEED;
-                        break;
-
-                    case KeyEvent.VK_DOWN:
-                        vY -= -SCROLL_SPEED;
-                        break;
-
-                    case KeyEvent.VK_LEFT:
-                        vX += -SCROLL_SPEED;
-                        break;
-
-                    case KeyEvent.VK_RIGHT:
-                        vX -= -SCROLL_SPEED;
-                        break;
-                }
-            }
-        });
     }
 
     public void resetTransform() {
         scale = 1;
         offX = offY = 0;
-        vX = vY = 0;
         dragging = false;
         grabX = grabY = 0;
-        dragX = dragY = 0;
     }
 
     public void applyTransform(Graphics2D g2d) {
         g2d.translate(offX, offY);
-        g2d.translate(dragX, dragY);
         g2d.scale(scale, scale);
     }
 
@@ -164,29 +103,35 @@ public abstract class NavigablePanel extends JPanel {
 
     private void doMousePressed(MouseEvent e) {
         notifyPressed(getActualX(e.getX()), getActualY(e.getY()));
-        dragging = true;
-        grabX = e.getX();
-        grabY = e.getY();
+        startDragging(e.getX(), e.getY());
     }
 
     private void doMouseReleased(MouseEvent e) {
         notifyReleased(getActualX(e.getX()), getActualY(e.getY()));
-        dragging = false;
+        stopDragging();
     }
 
     private void doMouseDragged(MouseEvent e) {
         notifyMoved(getActualX(e.getX()), getActualY(e.getY()));
         if (dragging) {
-            dragX = e.getX() - grabX;
-            dragY = e.getY() - grabY;
-            repaint();
+            updateDragging(e.getX(), e.getY());
         }
     }
 
-    private void updateOffset() {
-        offX += vX;
-        offY += vY;
+    private void startDragging(int x, int y) {
+        dragging = true;
+        grabX = x - offX;
+        grabY = y - offY;
+    }
+
+    private void updateDragging(int x, int y) {
+        offX = x - grabX;
+        offY = y - grabY;
         repaint();
+    }
+    
+    private void stopDragging() {
+        dragging = false;
     }
 
     private int getActualX(int x) {
