596f452ac3aaf9d688f02748cb718880fbde5377
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-11-25 02:12:33 +0100

    Add scale factor

--- OptiFret/src/view/DeliveryMap.java
+++ OptiFret/src/view/DeliveryMap.java
@@ -1,12 +1,13 @@
-/*
- */
 package view;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.lang.ref.WeakReference;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -27,15 +28,18 @@ public class DeliveryMap extends JPanel {
     private WeakReference<NodeView> selectedNode;
     private int maxX = 0;
     private int maxY = 0;
-    private final CopyOnWriteArrayList<Listener> listeners;
+    private CopyOnWriteArrayList<Listener> listeners;
+    private static final double SCALE_MAX = 3;
+    private static final double SCALE_MIN = 0.1;
+    private static final double SCALE_INC = 0.1;
+    public static final int PADDING = 20;
+
+    private double scale = 1;
 
     public DeliveryMap() {
         super();
-        this.listeners = new CopyOnWriteArrayList<>();
-        this.setDoubleBuffered(true);
-        mapArcs = new LinkedHashMap<>();
-        mapNodes = new LinkedHashMap<>();
-        selectedNode = new WeakReference<>(null);
+        setDoubleBuffered(true);
+        reset();
         addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -43,17 +47,11 @@ public class DeliveryMap extends JPanel {
 
             @Override
             public void mousePressed(MouseEvent e) {
-                System.out.println(String.valueOf(e.getX()));
-                System.out.println(String.valueOf(e.getY()));
-
                 notifyPressed(e);
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                System.out.println(String.valueOf(e.getX()));
-                System.out.println(String.valueOf(e.getY()));
-
                 notifyReleased(e);
             }
 
@@ -75,7 +73,20 @@ public class DeliveryMap extends JPanel {
                 notifyMoved(e);
             }
         });
+        addMouseWheelListener(new MouseWheelListener() {
 
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                notifyScrolled(e);
+            }
+        });
+    }
+
+    private void reset() {
+        listeners = new CopyOnWriteArrayList<>();
+        mapArcs = new LinkedHashMap<>();
+        mapNodes = new LinkedHashMap<>();
+        selectedNode = new WeakReference<>(null);
     }
 
     public void setSelectedNodeById(Long id) {
@@ -94,6 +105,7 @@ public class DeliveryMap extends JPanel {
 
     public void updateNetwork(List<RoadNode> nodes) {
         if (nodes == null) {
+            reset();
             return;
         }
         mapArcs.clear();
@@ -120,7 +132,12 @@ public class DeliveryMap extends JPanel {
                 }
             }
         }
-        this.setPreferredSize(new Dimension(maxX + 20, maxY + 20));
+        setPreferredSize(new Dimension(maxX + PADDING, maxY + PADDING));
+    }
+
+    @Override
+    public void setPreferredSize(Dimension preferredSize) {
+        super.setPreferredSize(preferredSize);
     }
 
     public void updateDeliveryNodes(List<RoadNode> nodes) {
@@ -154,8 +171,10 @@ public class DeliveryMap extends JPanel {
         System.out.println("Début parcours nodes Pressed");
         boolean voidClic = true;
         for (NodeView node : mapNodes.values()) {
-            if (!node.onMouseDown(e.getX(), e.getY())) {
-                voidClic = false;
+            int x = getActualX(e.getX());
+            int y = getActualY(e.getY());
+            if (!node.onMouseDown(x, y)) {
+                voidClic = false; // TODO also break loop ?
             }
         }
         if (voidClic) {
@@ -164,29 +183,51 @@ public class DeliveryMap extends JPanel {
             }
         }
         fireChangeEvent();
-        System.out.println("Fin parcours nodes Pressed");
         repaint();
     }
 
     private void notifyReleased(MouseEvent e) {
-        System.out.println("Début parcours nodes Released");
+        int x = getActualX(e.getX());
+        int y = getActualY(e.getY());
         for (NodeView node : mapNodes.values()) {
-            node.onMouseUp(e.getX(), e.getY());
+            node.onMouseUp(x, y);
         }
-        System.out.println("Fin parcours nodes Released");
     }
 
     private void notifyMoved(MouseEvent e) {
+        int x = getActualX(e.getX());
+        int y = getActualY(e.getY());
         for (NodeView node : mapNodes.values()) {
-            node.onMouseOver(e.getX(), e.getY());
+            node.onMouseOver(x, y);
         }
         repaint();
+    }
+
+    private void notifyScrolled(MouseWheelEvent e) {
+        double diff = SCALE_INC * (double) e.getWheelRotation();
+        if (diff < 0) {
+            scale = Math.min(SCALE_MAX, scale - diff);
+        } else {
+            scale = Math.max(SCALE_MIN, scale - diff);
+        }
+        setSize(new Dimension(maxX + PADDING, maxY + PADDING));
+        repaint();
+    }
+
+    private int getActualX(int x) {
+        return (int) (x / scale);
+    }
+
+    private int getActualY(int y) {
+        return (int) (y / scale);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawRect(2, 2, getWidth() - 5, getHeight() - 5);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.scale(scale, scale);
         draw(g);
     }
 
@@ -205,6 +246,17 @@ public class DeliveryMap extends JPanel {
 
     public void setSelectedNode(WeakReference<NodeView> selectedNode) {
         this.selectedNode = selectedNode;
+    }
+
+    public double getScale() {
+        return scale;
+    }
+
+    public void setScale(double scale) {
+        if (scale <= 0) {
+            throw new IllegalArgumentException("'scale' donnée ne doit pas être <= 0");
+        }
+        this.scale = scale;
     }
 
     public int getMaxX() {
