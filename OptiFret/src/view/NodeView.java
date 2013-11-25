ffe5358533ec7c40904456e757a4a8a713d8b605
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-11-25 17:32:45 +0100

    Fix ugly nodeview background (arcs no longer visible inside node)

--- OptiFret/src/view/NodeView.java
+++ OptiFret/src/view/NodeView.java
@@ -9,7 +9,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.lang.ref.WeakReference;
 
@@ -22,72 +21,72 @@ public class NodeView {
     public static final int DIAMETER = 10;
     private static final int STROKE = 2;
     private static final int STROKE_ENLIGHT = 5;
-    private int x1;
-    private int y1;
-    private Long address;
 
-   
     private final BasicStroke myStroke = new BasicStroke(STROKE);
     private final BasicStroke myStrokeEnlight = new BasicStroke(STROKE_ENLIGHT);
-    private Ellipse2D circle;
+    private final Ellipse2D circle;
     private final Color cBasic = new Color(100, 100, 100);
     private final Color cSelectedBasic = new Color(255, 204, 0);
     private final Color cBasicDel = new Color(0, 51, 102);
     private final Color cSelectedBasicDel = new Color(255, 204, 100);
     private final Color cMouseOver = new Color(204, 204, 204);
+
+    private final WeakReference<DeliveryMap> parent;
+
+    private int x1;
+    private int y1;
+
+    private final Long address;
     private boolean mouseOver = false;
     private boolean selected = false;
-    private WeakReference<DeliveryMap> parent;
 
     public enum MODE {
 
         CLASSIC, DELIVERY
     };
+
     private MODE mode;
 
-    public NodeView(int x1, int y1,Long address, WeakReference<DeliveryMap> ref, MODE mode) {
+    public NodeView(int x1, int y1, Long address, WeakReference<DeliveryMap> ref, MODE mode) {
         this.x1 = x1;
         this.y1 = y1;
-        this.address=address;
-        this.circle = new Ellipse2D.Double((float) x1, (float) y1, (float) DIAMETER, (float) DIAMETER);
+        this.address = address;
         this.mode = mode;
+        circle = new Ellipse2D.Double((float) x1, (float) y1,
+                (float) DIAMETER, (float) DIAMETER);
         parent = ref;
     }
 
     @Override
     public boolean equals(Object object) {
-        boolean sameSame = false;
-
         if (object != null && object instanceof NodeView) {
-            if (this.x1 == ((NodeView) object).x1 && this.y1 == ((NodeView) object).y1) {
-                sameSame = true;
+            if (x1 == ((NodeView) object).x1 && y1 == ((NodeView) object).y1) {
+                return true;
             }
         }
-
-        return sameSame;
+        return false;
     }
 
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 41 * hash + this.x1;
-        hash = 47 * hash + this.y1;
+        hash = 41 * hash + x1;
+        hash = 47 * hash + y1;
         return hash;
     }
 
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g.create();
-        g2d.setRenderingHint(
-                RenderingHints.KEY_ANTIALIASING,
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(
-                RenderingHints.KEY_TEXT_ANTIALIASING,
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g2d.setRenderingHint(
-                RenderingHints.KEY_FRACTIONALMETRICS,
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
                 RenderingHints.VALUE_FRACTIONALMETRICS_ON);
         g2d.setStroke(myStroke);
         g2d.translate(-DIAMETER / 2, -DIAMETER / 2);
+        g2d.setColor(parent.get().getBackground());
+        g2d.fill(circle);
         switch (mode) {
             case CLASSIC:
                 if (selected) {
@@ -106,18 +105,15 @@ public class NodeView {
                 }
                 break;
         }
+
         if (mouseOver) {
             g2d.setColor(cMouseOver);
             g2d.setStroke(myStrokeEnlight);
             g2d.draw(circle);
             g2d.setStroke(myStroke);
-
         } else {
             g2d.draw(circle);
-
         }
-
-        g2d.translate(DIAMETER / 2, DIAMETER / 2);
     }
 
     public int getX1() {
@@ -149,16 +145,16 @@ public class NodeView {
     }
 
     public boolean onMouseDown(int x, int y) {
-        boolean voidClic=true;
+        boolean voidClic = true;
         if (circle.contains(x + DIAMETER / 2, y + DIAMETER / 2) && (parent.get() != null)) {
             selected = true;
-            voidClic=false;
+            voidClic = false;
             if (parent.get().getSelectedNode() != null) {
                 parent.get().getSelectedNode().clear();
             }
             parent.get().setSelectedNode(new WeakReference<>(this));
         } else {
-            selected = false;            
+            selected = false;
         }
         return voidClic;
     }
@@ -167,11 +163,7 @@ public class NodeView {
     }
 
     public void onMouseOver(int x, int y) {
-        if (circle.contains(x + DIAMETER / 2, y + DIAMETER / 2) && (parent.get() != null)) {
-            mouseOver = true;
-        } else {
-            mouseOver = false;
-        }
+        mouseOver = circle.contains(x + DIAMETER / 2, y + DIAMETER / 2) && (parent.get() != null);
     }
 
     public MODE getMode() {
@@ -181,8 +173,8 @@ public class NodeView {
     public void setMode(MODE mode) {
         this.mode = mode;
     }
-    
-     public Long getAddress() {
+
+    public Long getAddress() {
         return address;
     }
 }
