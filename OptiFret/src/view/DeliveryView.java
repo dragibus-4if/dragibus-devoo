babf694ae93784ebcaf1b5face64811e2b2bd58e
quentin-coursodon <quentin.coursodon@gmail.com>
2013-11-28 17:28:07 +0100

    add some fucking javadoc

--- OptiFret/src/view/DeliveryView.java
+++ OptiFret/src/view/DeliveryView.java
@@ -23,15 +23,14 @@ import org.jdesktop.swingx.JXImageView;
 import org.jdesktop.swingx.VerticalLayout;
 
 /**
- *
+ *  Vue correspondant à l'affichage d'une livraison 
+ *  sous la forme d'un bandeau déroulant
  * @author Quentin
  */
 public class DeliveryView extends JPanel {
 
     private static final Color MINIMAL_BG_COLOR = new Color(0, 0, 0, 0);
-    private static final Color MINIMAL_BG_OVER_COLOR = new Color(240, 240, 240, 175);
     private static final Color MINIMAL_BG_SELECT_COLOR = new Color(0, 0, 0, 40);
-    private static final Color MINIMAL_BG_UNCOLLAPSED_COLOR = new Color(200, 200, 200, 100);
     private static final Color MINIMAL_BG_LATE_COLOR = new Color(215, 20, 20, 40);
     private static final Color MINIMAL_BG_LATE_SELECT_COLOR = new Color(215, 20, 20, 80);
     private final Delivery delivery;
@@ -50,6 +49,13 @@ public class DeliveryView extends JPanel {
     private static final String ARROW_UP = "\u2191";
     private static final String ARROW_DOWN = "\u2193";
 
+    /**
+     * Constructeur utilisant une Delivery 
+     * et la Deliverylist parente
+     * 
+     * @param delivery
+     * @param parent 
+     */
     public DeliveryView(Delivery delivery, DeliveryList parent) {
         super(new VerticalLayout());
         folded = true;
@@ -84,6 +90,7 @@ public class DeliveryView extends JPanel {
 
         add(makeMinimal());
         add(makeExtend());
+        //Listerners de changement de d'état du JXCollapsiblePane
         extend.addPropertyChangeListener("collapsed", new PropertyChangeListener() {
             @Override
             public void propertyChange(PropertyChangeEvent evt) {
@@ -96,12 +103,21 @@ public class DeliveryView extends JPanel {
         repaint();
     }
 
+    /**
+     * Permet de valider et repeindre la Deliverylist contenant la DeliveryView
+     */
     private void repaintall() {
         this.parent.validate();
         this.parent.repaint();
 
     }
-
+    /**
+     * Méthode permettant d'initialiser le JXcollapsiblePane extend
+     * Cette méthode ne devrait pas etre appeller autrement que par le constructeur
+     * 
+     * @see DeliveryView
+     * @return JXCollapsiblePane - renvoie le JXcollapsiblePane extend initialisé
+     */
     private JXCollapsiblePane makeExtend() {
         extend.setLayout(new GridLayout(5, 1));
         extend.add(clientNameLabel);
@@ -110,6 +126,7 @@ public class DeliveryView extends JPanel {
         extend.add(timeSlotHour);
         extend.add(predHour);
 
+        //Met en place le Background
         extend.setBackground(MINIMAL_BG_COLOR);
         extend.getContentPane().setBackground(MINIMAL_BG_COLOR);
         for (Component c : extend.getComponents()) {
@@ -121,9 +138,13 @@ public class DeliveryView extends JPanel {
         return extend;
     }
 
+    /**
+     * Méthode permettant d'initialiser le JPanel minimal
+     * Cette méthode ne devrait pas etre appeller autrement que par le constructeur
+     * 
+     * @return Jpanel - renvoie le JPanel minimal initialisé
+     */
     private JPanel makeMinimal() {
-
-
         minimal.setBackground(MINIMAL_BG_COLOR);
         minimal.setOpaque(true);
         if (deliveryLate) {
@@ -141,8 +162,10 @@ public class DeliveryView extends JPanel {
         minimal.add(toggleButton);
         minimal.add(idDeliveryLabel);
         final DeliveryView that = this;
+        //MouseListerner
         minimal.addMouseListener(new MouseListener() {
             @Override
+            //Selection par clic
             public void mouseClicked(MouseEvent e) {
                 select();
             }
@@ -156,23 +179,15 @@ public class DeliveryView extends JPanel {
             }
 
             @Override
+            //Mise en surbrillance lorsque la souris est au dessus
             public void mouseEntered(MouseEvent e) {
-
-
-                //that.minimal.setBackground(MINIMAL_BG_OVER_COLOR);
                 that.minimal.setBorder(BorderFactory.createLineBorder(Color.gray, 1, false));
                 repaintall();
-
             }
-
             @Override
             public void mouseExited(MouseEvent e) {
-
-
-                //that.minimal.setBackground(MINIMAL_BG_COLOR);
                 that.minimal.setBorder(null);
                 repaintall();
-
             }
         });
         toggleButton.addMouseListener(new MouseListener() {
@@ -197,12 +212,13 @@ public class DeliveryView extends JPanel {
             public void mouseExited(MouseEvent e) {
             }
         });
-
         minimal.validate();
         minimal.repaint();
         return minimal;
     }
-
+    /**
+     * Déclenche le depliement ou repliement du JXCollapsiblePane extend
+     */
     public void toggle() {
         toggle(new MouseEvent(this, 0, 0, 0, 0, 0, 0, false, 0));
     }
@@ -212,12 +228,13 @@ public class DeliveryView extends JPanel {
                 .actionPerformed(new ActionEvent(e, WIDTH, TOOL_TIP_TEXT_KEY));
         updateToggleButton();
         folded = !folded;
-
-
         validate();
         repaint();
     }
-
+    /**
+     * Met à jour l'affichage du bouton de déroulement en fonction de l'état
+     * du JXCollapsiblePane
+     */
     private void updateToggleButton() {
         if (folded) {
             toggleButton.setText(ARROW_DOWN);
@@ -228,6 +245,10 @@ public class DeliveryView extends JPanel {
         minimal.repaint();
     }
 
+    /**
+     * Permet de séléctionner la DeliveryView sans envoyé de notification de changement
+     * d'état aux listeners de la deliveryList parent
+     */
     public void onEventSelect() {
         if(deliveryLate){
             minimal.setBackground(MINIMAL_BG_LATE_SELECT_COLOR);
@@ -239,7 +260,10 @@ public class DeliveryView extends JPanel {
         parent.setSelected(this);
 
     }
-
+    /**
+     * Permet de séléctionner la DeliveryView dans la DeliveryList.
+     * en envoyant une notification de changement d'état au Listerners de la DeliveryList parent
+     */
     public void select() {
         if(deliveryLate){
             minimal.setBackground(MINIMAL_BG_LATE_SELECT_COLOR);
@@ -251,9 +275,10 @@ public class DeliveryView extends JPanel {
         parent.setSelected(this);
 
         parent.fireChangeEvent();
-
     }
-
+    /**
+     * Permet de déselectionner la DeliveryView
+     */
     public void unselect() {
         if(deliveryLate){
             minimal.setBackground(MINIMAL_BG_LATE_COLOR);
@@ -262,7 +287,6 @@ public class DeliveryView extends JPanel {
         minimal.setBackground(MINIMAL_BG_COLOR);
         }
         repaintall();
-
     }
 
     public Delivery getDelivery() {
